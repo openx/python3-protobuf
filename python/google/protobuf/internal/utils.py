22d92bd965a8300e15d8145a380c7908c9d7d5a0
Charles Law <charles.law@openx.com>
2013-03-20 14:02:49 -0700

    py2 - clean up conversion functions

--- python/google/protobuf/internal/utils.py
+++ python/google/protobuf/internal/utils.py
@@ -1,5 +1,19 @@
 from __future__ import unicode_literals
 
+"""
+This module abstracts all the bytes<-->string conversions so that the python 2
+and 3 code everywhere else is similar.  This also has a few simple functions
+that deal with the fact that bytes are different between the 2 versions even
+when using from __future__ import unicode_literals.  For example:
+Python 2:
+    b'mybytes'[0] --> b'm' (type str)
+Python 3:
+    b'mybytes'[0] --> 109 (type int)
+So in some places we get an index of a bytes object and we have to make sure
+the behaviour is the same in both versions of python so functions here take
+care of that.
+"""
+
 try:
     import cmp
 except ImportError:
@@ -7,29 +21,68 @@ except ImportError:
     def cmp(a, b):
         return (a > b) - (a < b)
 
-def bytes_to_string(byte_array):
-    return ''.join([b for b in byte_array])
-
 def string_to_bytes(text):
+    """
+    Convert a string to a bytes object.  This is a raw conversion
+    so that ord() on each element remains unchanged.
+    Input type: string
+    Output type: bytes
+    """
     return b"".join([c for c in text])
 
-def bytestr_to_string(bytestr):
-    return unicode(bytestr, 'utf-8')
+def bytes_to_string(byte_array):
+    """
+    Inverse of string_to_bytes.
+    """
+    return ''.join([b for b in byte_array])
 
 def string_to_bytestr(string):
-    #return b''.join([chr(b) for b in string.encode('utf-8')])
+    """
+    Convert a string to a bytes object.  This encodes the object as well which
+    will typically change ord on each element & change the length (i.e. 1 char
+    could become 1/2/3 bytes)
+    """
     return string.encode('utf-8')
 
+def bytestr_to_string(bytestr):
+    """
+    Inverse of string_to_bytestr.
+    """
+    return unicode(bytestr, 'utf-8')
+
 def byte_ord(bytes):
+    """
+    This converts a *single* input byte to an integer.  Usually used in
+    conjuction with b'mybytes'[i].  See module description.
+    Input: 2: string/pseudo-bytes 3: int
+    Output: int
+    """
     return ord(bytes)
 
 def char_byte(char):
+    """
+    Convert a *single* byte to an int.  Usually used like char(b'm').  See
+    module description.
+    Input: 2: string/pseudo-bytes 3: bytes
+    Output: 2: string/pseudo-bytes 3: int
+    """
     return char
 
 def bytestr(val):
-    """Pass in an integer val"""
+    """
+    Convert a *single* integer to a bytes object.  Usually used like
+    bytestr(int).
+    Input: int
+    Output: bytes
+    """
     return chr(val)
 
 def single_byte(byte_val):
+    """
+    Convert a *single* byte to a byte.  This is usually used in conjuction with
+    b'mybytes'[i].  See module description.
+    Input: 2: string/pseudo-bytes 3: int
+    Output: bytes
+    """
     return byte_val
 
